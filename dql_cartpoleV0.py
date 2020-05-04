936156ea044f6f2e48f19aa72d2879898f9afdaa
panargirakis <panargirakis@gmail.com>
2020-05-04 17:48:45 -0400

    gitignore

--- dql_cartpoleV0.py
+++ dql_cartpoleV0.py
@@ -48,7 +48,7 @@ dqn.compile(Adam(lr=1e-3), metrics=['mae'])
 dqn.fit(env, nb_steps=50000, visualize=False, verbose=2)
 
 # After training is done, we save the final weights.
-dqn.save_weights('dqn_{}_weights.h5f'.format(ENV_NAME), overwrite=True)
+dqn.save_weights('checkpoints/dqn_{}_weights.h5f'.format(ENV_NAME), overwrite=True)
 
 # Finally, evaluate our algorithm for 5 episodes.
 dqn.test(env, nb_episodes=5, visualize=True)
