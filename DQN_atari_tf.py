e089e236fbe22b71d1d356861ccf6d33746d877a
panargirakis <panargirakis@gmail.com>
2020-05-12 00:45:44 -0400

    more models

--- DQN_atari_tf.py
+++ DQN_atari_tf.py
@@ -129,6 +129,7 @@ import random
 import gym
 import tensorflow.compat.v1 as tf
 tf.disable_v2_behavior()
+# import tensorflow as tf
 import numpy as np
 import imageio
 from skimage.transform import resize
